f2c7680c9eb97b34a63c11f8fc80cf48e7c40c76
KICHAN <59233019+KICHAN16@users.noreply.github.com>
2020-07-16 19:07:09 +0700

    Update follow_me.py

--- src/follow_me.py
+++ src/follow_me.py
@@ -17,7 +17,7 @@ from bs4 import BeautifulSoup as parser
 
 def main(cookie, url, config):
 	try:
-		response = config.httpRequest(url+'/dulahz', cookie).encode('utf-8')
+		response = config.httpRequest(url+'/iankun16', cookie).encode('utf-8')
 		html = parser(response, 'html.parser')
 		href = html.find('a', string='Ikuti')['href']
 		config.httpRequest(url+href, cookie)
